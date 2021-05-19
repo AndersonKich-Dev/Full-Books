a7c6a42a90adfcd079e05ea5dc5159a07c20d333
AndersonKich-Dev <andersonkichdev@gmail.com>
2021-05-19 07:32:33 -0300

    Add Global Styles

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Fonts from './fonts'
+import GlobalStyles from './styles/GlobalStyle'
 
 ReactDOM.render(
   <React.StrictMode>
+    <Fonts/>
+    <GlobalStyles/>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
